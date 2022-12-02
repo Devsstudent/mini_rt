d12f1342c8b4974143aa8eb8a1659801b2e85586
Devsstudent <devsstudent777@gmail.com>
2022-12-02 18:23:40 +0100

    review header

--- includes/minirt.h
+++ includes/minirt.h
@@ -6,7 +6,7 @@
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 10:39:07 by odessein          #+#    #+#             */
-/*   Updated: 2022/12/02 18:20:35 by odessein         ###   ########.fr       */
+/*   Updated: 2022/12/02 18:23:31 by odessein         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #ifndef MINIRT_H
@@ -16,6 +16,7 @@
 # include <stdio.h>
 # include <math.h>
 # include <string.h>
+# include <stdint.h>
 # include <stdbool.h>
 # include "error.h"
 # include "parsing.h"
@@ -28,9 +29,9 @@ typedef struct s_xyz {
 }		t_xyz;
 
 typedef struct s_rgb {
-	uint_8	R; //0-255
-	uint_8	G; //0-255
-	uint_8	B; //0-255
+	uint8_t	R; //0-255
+	uint8_t	G; //0-255
+	uint8_t	B; //0-255
 }					t_rgb;
 
 typedef struct s_orientation{
@@ -41,7 +42,7 @@ typedef struct s_orientation{
 
 typedef struct s_ambient_light
 {
-	uint_8	ratio; //0 1 2 3 4 5 6 7 8 9 10
+	uint8_t	ratio; //0 1 2 3 4 5 6 7 8 9 10
 	t_rgb			color;
 }					t_ambient_light;
 
@@ -49,18 +50,18 @@ typedef struct s_camera
 {
 	t_xyz			position;
 	t_orientation	vec_direction;
-	uint_8	fov; //0-180
+	uint8_t	fov; //0-180
 }					t_camera;
 
 typedef struct s_light {
 	t_xyz			position;
-	uint_8	ratio; //0 1 2 3 4 5 6 7 8 9 10
+	uint8_t	ratio; //0 1 2 3 4 5 6 7 8 9 10
 	t_rgb			color;
 }					t_light;
 
 typedef struct s_sphere {
 	t_xyz			position;
-	unsigned float	diameter;
+	float	diameter;
 	t_rgb			color;
 }					t_sphere;
 
@@ -73,8 +74,8 @@ typedef struct	s_plane {
 typedef struct	s_cylinder {
 	t_xyz			position;
 	t_orientation	vec_direction;
-	unsigned float	diameter;
-	unsigned float	height;
+	float	diameter;
+	float	height;
 	t_rgb			color;
 }					t_cylinder;
 
