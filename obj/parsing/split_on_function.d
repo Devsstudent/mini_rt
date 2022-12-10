c8f3aa98fc767f3790b475195351c06fbee3d54a
Maha BELRHAZI <mbelrhaz@e1r6p11.clusters.42paris.fr>
2022-12-10 21:59:08 +0100

    created a file for vector operations, added the computing of the unit vector for the screen width

---
+++ obj/parsing/split_on_function.d
@@ -0,0 +1,4 @@
+obj/parsing/split_on_function.o: src/parsing/split_on_function.c \
+  src/parsing/parsing.h src/parsing/../../includes/minirt.h \
+  lib/mlx/mlx.h lib/mlx/mlx_int.h src/error/error.h includes/minirt.h \
+  lib/libft/libft.h src/window/window.h
--- src/window/sphere_equation.c
+++ src/window/sphere_equation.c
@@ -6,7 +6,7 @@
 /*   By: odessein <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 15:05:56 by odessein          #+#    #+#             */
-/*   Updated: 2022/12/09 16:24:15 by odessein         ###   ########.fr       */
+/*   Updated: 2022/12/10 18:37:56 by mbelrhaz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "window.h"
@@ -34,82 +34,6 @@
 //yindent = vireplandHeight / (float)yRes; (yRes = 1080) (1920 * 1080 resoltion)
 //Ces valeurs dépdendent du fov (field of vision), de la caméra, cependant nous allons assummer pour le moment que la largeur est de 0.35, la hauteur de 0.5 et la distance de 1.0. ?? How to define thos variable from the fov ..
 
-
-
-t_xyz	get_vec_vertical(t_xyz original_pos)
-{
-	float	unknow;
-	t_xyz	res;
-
-	res.x = 0;
-	res.y = 0;
-	res.z = 0;
-	if (original_pos.x == 0 && original_pos.y == 0 && original_pos.z == 0)
-	{
-		write(2, "Not possible to get the orthogonal vector\n", ft_strlen("Not possible to get the orthogonal vector\n"));
-		return (res);
-	}
-	if (original_pos.z != 0)
-	{
-		unknow = original_pos.z;
-		res.z = (-((2) * original_pos.x) / original_pos.z - (( 4 * original_pos.y) / original_pos.z));
-		res.x = 2;
-		res.y = 4;
-	}
-	else if (original_pos.y != 0)
-	{
-		res.y = (-((2) * original_pos.x) / original_pos.y - (( 4 * original_pos.z) / original_pos.y));
-		res.x = 2;
-		res.z = 4;
-	}
-	else if (original_pos.x != 0)
-	{
-		res.x = (-((2) * original_pos.y) / original_pos.x - (( 4 * original_pos.z) / original_pos.x));
-		res.y = 2;
-		res.z = 4;
-	}
-	return (res);
-}
-
-t_xyz	get_oppo(t_xyz right_pos)
-{
-	t_xyz	res;
-
-	res.x = -right_pos.x;
-	res.y = -right_pos.y;
-	res.z = -right_pos.z;
-	return (res);
-}
-
-t_xyz	get_vec_horizontal(t_xyz v_director, t_xyz v_ortho)
-{
-	t_xyz	res;
-
-	res.x = v_director.y * v_ortho.z - v_director.z * v_ortho.y;
-	res.y = v_director.z * v_ortho.x - v_director.x * v_ortho.z;
-	res.z = v_director.x * v_ortho.y - v_director.y * v_ortho.x;
-	return (res);
-}
-
-float	norm_of_vector(t_xyz vector)
-{
-	float	res;
-
-	res = sqrt(pow(vector.x, 2) + pow(vector.y, 2) + pow(vector.z, 2));
-	return (res);
-}
-
-void	normalizing(t_xyz *vector, float vector_norm, float window_scale)
-{
-	if (window_scale == 0 || vector_norm == 0)
-	{
-		ft_putstr_fd("Error dividing by zero in normalizing\n", 2);
-		return ;
-	}
-	vector->x = (vector->x / (window_scale * vector_norm));
-	vector->y = (vector->y / (window_scale * vector_norm));
-	vector->z = (vector->z / (window_scale * vector_norm));
-}
 
 void	render_window(t_objects objs)
 {
