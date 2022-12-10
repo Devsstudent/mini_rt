08660739ac340fdbfbde6dcf94235d1d96504d4f
Maha BELRHAZI <mbelrhaz@e1r6p11.clusters.42paris.fr>
2022-12-10 22:00:21 +0100

    added func get_unit_horizontal_vector to move horizontally on the screen

--- obj/parsing/elem_structure.d
+++
@@ -1,4 +0,0 @@
-obj/parsing/elem_structure.o: src/parsing/elem_structure.c \
-  includes/minirt.h lib/mlx/mlx.h lib/mlx/mlx_int.h src/error/error.h \
-  src/parsing/parsing.h src/parsing/../../includes/minirt.h \
-  lib/libft/libft.h src/window/window.h
--- obj/parsing/fill_elems.d
+++
@@ -1,4 +0,0 @@
-obj/parsing/fill_elems.o: src/parsing/fill_elems.c src/parsing/parsing.h \
-  src/parsing/../../includes/minirt.h lib/mlx/mlx.h lib/mlx/mlx_int.h \
-  src/error/error.h includes/minirt.h lib/libft/libft.h \
-  src/window/window.h
