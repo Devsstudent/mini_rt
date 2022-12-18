201ae297a1069cef76b37a74c5c2fce02fb0b981
Devsstudent <user@disp3269>
2022-12-18 23:00:31 +0100

    save starting rgb color etc

--- obj/parsing/split_on_function.d
+++
@@ -1,4 +0,0 @@
-obj/parsing/split_on_function.o: src/parsing/split_on_function.c \
- src/parsing/parsing.h src/parsing/../../includes/minirt.h lib/mlx/mlx.h \
- lib/mlx/mlx_int.h src/error/error.h includes/minirt.h \
- src/parsing/parsing.h lib/libft/libft.h src/window/window.h
