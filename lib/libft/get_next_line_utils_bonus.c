16737d99d1e2a2eb96ad499b6490df92306bad10
Hanako <hanako@MacBook-Pro-de-Hanako.local>
2022-12-29 14:22:12 +0100

    code compiling on mac (rm bzero from libft && usless variable)

--- lib/libft/get_next_line_utils_bonus.c
+++ lib/libft/get_next_line_utils_bonus.c
@@ -72,17 +72,3 @@ size_t	ft_len_till_nl(const char *s)
 		size++;
 	return (size);
 }
-
-void	ft_bzero(void *s, size_t n)
-{
-	size_t	i;
-	char	*temp;
-
-	i = 0;
-	temp = (char *) s;
-	while (i < n)
-	{
-		temp[i] = '\0';
-		i++;
-	}
-}
