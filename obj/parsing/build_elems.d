5ea00c1efbde5637b6bfc223ee04d5d27a4a4d28
Maha BELRHAZI <mbelrhaz@e1r7p10.clusters.42paris.fr>
2023-01-07 18:31:53 +0100

    merge

--- obj/parsing/build_elems.d
+++
@@ -1,4 +0,0 @@
-obj/parsing/build_elems.o: src/parsing/build_elems.c \
-  src/parsing/parsing.h src/parsing/../../includes/minirt.h \
-  lib/mlx/mlx.h lib/mlx/mlx_int.h src/error/error.h includes/minirt.h \
-  lib/libft/libft.h src/window/window.h
